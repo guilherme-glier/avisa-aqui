6dd120b9c298c53c0f0ab28cbc4b7a83c4e7b754
Guilherme Glier <guiglier10@gmail.com>
2024-10-10 19:21:24 -0300

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main>
     <header>
       <nav>
+        <img src="../assets/logo.png" alt="Logo" class="logo" />
         <ul>
           <template v-if="!isAuthenticated">
             <li><RouterLink to="cadastro">Cadastro</RouterLink></li>
@@ -25,7 +26,7 @@
 <script setup>
 import { RouterLink, useRouter } from 'vue-router';
 import { checkAuthenticationStatus } from '../router';
-import Map from '../components/Map.vue'
+import Map from '../components/Map.vue';
 
 const router = useRouter();
 const isAuthenticated = checkAuthenticationStatus();
@@ -34,3 +35,55 @@ function logout() {
   localStorage.removeItem('userData');
 }
 </script>
+
+<style>
+main {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #ffffff;
+  color: #565173;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 50px;
+  margin-right: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  gap: 15px;
+  margin: 0;
+  padding: 0;
+}
+
+nav a {
+  color: #565173;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+nav button {
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+nav button:hover {
+  text-decoration: underline;
+}
+
+</style>
