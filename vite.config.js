56487ead49ccabe2e01282b0ca25f9e1076a8ac7
Tiago Portaluppi <tiago.portaluppi@universo.univates.br>
2024-12-10 04:24:56 -0300

    chore: server https

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,15 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   https: true,
-  // },
+  server: {
+    https: true,
+  },
   plugins: [
     vue(),
-    // mkcert({
-    //   force: true,
-    //   autoUpgrade: true,
-    // }),
+    mkcert({
+      force: true,
+      autoUpgrade: true,
+    }),
     nodePolyfills(),
   ],
   resolve: {
